d7bb6a14ec61b81c88ce5ec9e10224da5cd1ad84
dewa.g.ari28 <dewa.g.ari28@gmail.com>
2021-05-26 10:49:35 +0700

    Created Page About and Portfolio using JavaScriipt

---
+++ .history/js/script_20210526100949.js
@@ -0,0 +1,29 @@
+
+/* ---------- About Tabs --------- */
+
+const tabsContainer = document.querySelector(".about-tabs"),
+aboutSection = document.querySelector(".about-section");
+
+tabsContainer.addEventListener("click", (e) => {
+    if(e.target.classList.contains("tab-item") && !e.target.classList.contains("active")){
+        tabsContainer.querySelector(".active").classList.remove("active");
+        e.target.classList.add("active");
+        const target = e.target.getAttribute("data-target");
+        aboutSection.querySelector(".tab-content.active").classList.remove("active");
+        aboutSection.querySelector(target).classList.add("active");
+    }
+});
+
+
+/* ------------- Portfolio Item Details Popup ------------------- */
+
+document.addEventListener("click", (e) => {
+    if(e.target.classList.contains("view-project-btn")){
+        togglePortfolioPopup();
+    }
+})
+
+function togglePortfolioPopup() {
+    document.querySelector(".porfolio-popup").classList.toggle("open")
+}
+
